b80ae705e996c79ee70fe396a8b663c190582687
Kanij Fatema <kfatema991@gmail.com>
2022-04-04 19:57:24 +0600

    first commit

---
+++ src/components/Reviews/Reviews.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Reviews.css';
+const Reviews = () => {
+    return (
+        <div>
+            <div className='reviews'>
+                <h2>Customer Reviews</h2>
+            </div>
+            <div className='cart'>
+
+            </div>
+        </div>
+    );
+};
+
+export default Reviews;
